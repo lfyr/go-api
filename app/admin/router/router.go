808d2f936f13bcf436486a5d36a0d0da5b7e4d05
OlQv+5.4 <chaoyi.wang>
2024-06-27 16:41:31 +0800

    bug

--- app/admin/router/router.go
+++ app/admin/router/router.go
@@ -2,11 +2,13 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/lfyr/go-api/middleware"
 	"github.com/lfyr/go-api/utils"
 )
 
 func Router() (router *gin.Engine) {
 	router = gin.New()
+	router.Use(middleware.Cors()) // 直接放行全部跨域请求
 
 	// 服务健康检测
 	router.GET("/admin/health", func(c *gin.Context) {
