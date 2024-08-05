5064d10661fe917c12e82769817b7519fd959121
OlQv+5.4 <chaoyi.wang>
2024-08-05 13:25:36 +0800

    bug

--- app/admin/router/router.go
+++ app/admin/router/router.go
@@ -4,17 +4,29 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/lfyr/go-api/middleware"
 	"github.com/lfyr/go-api/utils"
+	"github.com/sirupsen/logrus"
 )
 
 func Router() (router *gin.Engine) {
-	router = utils.GetNewGinEngine(true)
+
+	if utils.GVA_CONFIG.System.Env == "test" { //开发者
+		logrus.SetFormatter(&logrus.TextFormatter{})
+		logrus.SetLevel(logrus.DebugLevel)
+	} else { //非开发者
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+	router = gin.New()
+	router.Use(middleware.LoggerWithWriter(true), middleware.Recovery())
 	router.Use(middleware.Cors()) // 直接放行全部跨域请求
-	router.Use(middleware.ParseToken())
-	router.Use(middleware.CheckPrivilege()) // 权限校验
+
 	// 服务健康检测
 	router.GET("/admin/health", func(c *gin.Context) {
 		utils.Ok(c)
 	})
+	router.Use(middleware.ParseToken())
+	router.Use(middleware.CheckPrivilege()) // 权限校验
+
 	// 内部功能
 	adminRouter := router.Group("/admin")
 	{
